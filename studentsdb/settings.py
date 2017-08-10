5f1a09d5effe06e2faa53d658c401b1d7d5fa2a8
”Daniil <lemk@ukr.net>
2017-08-10 22:29:50 +0300

    heroku

--- studentsdb/settings.py
+++ studentsdb/settings.py
@@ -111,8 +111,8 @@ WSGI_APPLICATION = 'studentsdb.wsgi.application'
 
 # Parse database configuration from $DATABASE_URL
 db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
-db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'] = dj_database_url.config()
+DATABASES['default']['CONN_MAX_AGE'] = 500
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
