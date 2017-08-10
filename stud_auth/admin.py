59dc28b1ca022105b1f7780962a1c4d7055f7022
”Daniil <lemk@ukr.net>
2017-08-10 22:41:02 +0300

    heroku

--- stud_auth/admin.py
+++ stud_auth/admin.py
@@ -1,11 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from django.contrib import admin
-from django.contrib.auth import admin as auth_admin
-from django.contrib.auth.models import User
-
 
 
 #replace exciting User admin form
 
-admin.site.register(User)
