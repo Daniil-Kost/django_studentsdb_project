f08d4cdf86668251b4bc83c31b5885437c1619a2
”Daniil <lemk@ukr.net>
2017-08-11 13:40:51 +0300

    hhh

--- studentsdb/settings.py
+++ studentsdb/settings.py
@@ -117,6 +117,9 @@ HEROKU_POSTGRESQL_ONYX_URL = 'postgres://...'
 
 DATABASES['default'] =  dj_database_url.config()
 
+# Enable Connection Pooling
+DATABASES['default']['ENGINE'] = 'django_postgrespool'
+
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
